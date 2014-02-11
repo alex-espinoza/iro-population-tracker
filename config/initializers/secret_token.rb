adc8232efc637926c83c365f48095e32e9198d83
Alex Espinoza <aespinoza@aspria.net>
2014-02-11 13:15:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IroPopulationTracker::Application.config.secret_key_base = 'b4ecb137d903b5e1773cfe1e0c771284aa09a7de2d65ee71771518e2cf6460a7d384671634c1322a8fb31d3ed962dab234137426f76ad0be2668f2893a7ebe1d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IroPopulationTracker::Application.config.session_store :cookie_store, key: '_iro-population-tracker_session'
