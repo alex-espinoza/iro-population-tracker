768b7340ce5e10b70bb07a733b233dc8f8bbd9c4
Alex Espinoza <aespinoza@aspria.net>
2014-02-11 16:15:09 -0500

    Create PopulationInformation most recent record methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def index
+    @classic_records = PopulationInformation.most_recent_classic_records(3)
+    @renewal_records = PopulationInformation.most_recent_renewal_records(3)
   end
 end
--- app/models/population_information.rb
+++ app/models/population_information.rb
@@ -2,4 +2,12 @@ class PopulationInformation < ActiveRecord::Base
   validates_presence_of :server
   validates_presence_of :online
   validates_presence_of :shops
+
+  def self.most_recent_classic_records(number=3)
+    where(:server => "classic").order("created_at desc").limit(number)
+  end
+
+  def self.most_recent_renewal_records(number=3)
+    where(:server => "renewal").order("created_at desc").limit(number)
+  end
 end
