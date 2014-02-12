a2742a4dbc475f1408cf3c9ce38e970a9a97ae6d
Alex Espinoza <aespinoza@aspria.net>
2014-02-11 22:26:34 -0500

    Update population information query methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
-    @classic_records = PopulationInformation.most_recent_classic_records(3)
-    @renewal_records = PopulationInformation.most_recent_renewal_records(3)
+    @classic_records = PopulationInformation.most_recent_classic_records(13)
+    @renewal_records = PopulationInformation.most_recent_renewal_records(13)
+    @population_chart_data = PopulationInformation.population_chart_data(@classic_records, @renewal_records)
   end
 end
--- app/models/population_information.rb
+++ app/models/population_information.rb
@@ -3,11 +3,24 @@ class PopulationInformation < ActiveRecord::Base
   validates_presence_of :online
   validates_presence_of :shops
 
-  def self.most_recent_classic_records(number=3)
+  def self.most_recent_classic_records(number=13)
     where(:server => "classic").order("created_at desc").limit(number)
   end
 
-  def self.most_recent_renewal_records(number=3)
+  def self.most_recent_renewal_records(number=13)
     where(:server => "renewal").order("created_at desc").limit(number)
+  end
+
+  def self.population_chart_data(classic_records, renewal_records)
+    combined_data = classic_records.zip(renewal_records)
+    chart_data = combined_data.map do |records|
+      {
+        :time => records[0].created_at.localtime,
+        :classic_online => records[0].online,
+        :classic_shops => records[0].shops,
+        :renewal_online => records[1].online,
+        :renewal_shops => records[1].shops
+      }
+    end
   end
 end
